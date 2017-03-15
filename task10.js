dcb6779aec5432589ab8e17c2d55ba4a5756bd22
dmitrypasiukevich <pasiukevich@gmail.com>
2017-03-16 00:08:53 +0300

    2
    2

--- task10.js
+++ task10.js
@@ -8,11 +8,14 @@ for (var i = 0; i < length; i++) {
     arr[i] = getRandomNumber(0, 100);
 }
 var sorti = arr.length - 1;
-for (var a = 0; a < sorti; a++)
-for (var j = 0; j < sorti - a; j++)
-if (arr[j] < arr[j + 1]) {
-    var min = arr[j];
-    arr[j] = arr[j + 1];
-    arr[j + 1] = min;
+for (var a = 0; a < sorti; a++) {
+    for (var j = 0; j < sorti - a; j++) {
+        if (arr[j] < arr[j + 1]) {
+            var min = arr[j];
+            arr[j] = arr[j + 1];
+            arr[j + 1] = min;
+        }
+    }
 }
+
 console.log(arr);
